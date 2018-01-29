42006d32532e01b0e8ae85153254868304dc0ca3
Djitsu <djitsu@gmail.com>
2018-01-29 18:14:37 +0100

    refactor code to use a check for row method

--- functionnal_tests.py
+++ functionnal_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table= self.browser.find_element_by_id('id_list_table')
+        rows= table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
 
@@ -38,11 +43,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-                '1: Buy peacock feathers', [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         
         # There is an item to invite you to add another item
         # you enter "Use peacock feathers to make a fly"
@@ -52,14 +53,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again an now you can see both items on the list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-                '1: Buy peacock feathers', [row.text for row in rows]
-        )
-        self.assertIn(
-                '2: Use peacock feathers to make a fly', [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Is the site will remember this list? you can see that website generate a unique 
         # Url for you -- some explanatory test to that effect
